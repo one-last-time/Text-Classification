c85dc1a0e174cb031ade82a14c631afaecde43f1
Shaffat Dipu <msdipu12@gmail.com>
2020-04-08 16:57:11 +0600

    saving the trained model

--- train.py
+++ train.py
@@ -26,19 +26,15 @@ for i in range(len(X)):
     review = re.sub(r'\s+', ' ', review)
     corpus.append(review)
 
-#creating BOW model
-
-from sklearn.feature_extraction.text import CountVectorizer
-vectorizer = CountVectorizer(max_features=50000, min_df=2, max_df=0.7, stop_words=stopwords.words('english'))
-X=vectorizer.fit_transform(corpus).toarray()
 
 #creating tf-idf model
 
-from sklearn.feature_extraction.text import TfidfTransformer
-transformer = TfidfTransformer()
-X=transformer.fit_transform(X)
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer= TfidfVectorizer(max_features=50000, min_df=2, max_df=0.7, stop_words=stopwords.words('english'))
+X=vectorizer.fit_transform(corpus)
 
 
+#spilt test and train data
 from sklearn.model_selection import train_test_split
 text_train,text_test,sent_train,sent_test = train_test_split(X, y, test_size=0.1, random_state=0)
 
@@ -60,3 +56,12 @@ cm = confusion_matrix(sent_test, sent_pred)
 print(cm)
 accurancy = (cm[0,0]+cm[1,1])/len(sent_test)
 print('accurancy is ', (accurancy*100), '%')
+
+#pickling the classifier
+
+with open('classifier.pickle','wb') as f:
+    pickle.dump(classifier,f)
+
+#pickling the tfidf vectorizer
+with open('tfidfmodel.pickle','wb') as f:
+    pickle.dump(vectorizer,f)
